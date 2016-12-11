4ba354db26de9f087063959533b662316fa8334e
bmondolot <bmondolot@gmail.com>
2016-12-11 19:20:29 +0100

    First tries in everything

---
+++ src/components/HomeView.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="top10-list">
+      <h2>The last quotes</h2>
+      <ul>
+          <li v-for="quote in quotes">
+              {{ quote.text }}
+          </li>
+      </ul>
+    </div>
+    <div class="top3-list">
+      <h2>Authors</h2>
+      <ul>
+        <li v-for="author in authors">
+          {{ author }}
+        </li>
+      </ul>
+    </div>
+    <div class="top3-list">
+      <h2>Quoted users</h2>
+      <ul>
+        <li v-for="quotedUser in quotedUsers">
+          {{ quotedUser }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      quotes: [],
+      authors: [],
+      quotedUsers: []
+    }
+  },
+  created () {
+    this.quotes = this.getQuotes()
+    this.authors = this.getAuthors()
+    this.quotedUsers = this.getQuotedUsers()
+  },
+  methods: {
+    getQuotes: function () {
+      this.$http.get('http://localhost:3000/quotes').then((response) => {
+        this.quotes = response.data
+      }, (response) => {
+        console.log('Quotes could not be loaded')
+      })
+    },
+    getAuthors: function () {
+      // TODO : back pas prêt
+    },
+    getQuotedUsers: function () {
+      // TODO : back pas prêt
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 { 
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import Resource from 'vue-resource'
+import HomeView from './components/HomeView.vue'
+import AuthorsView from './components/AuthorsView.vue'
+
+// install router
+Vue.use(VueRouter)
+
+// install Resource to use ajax functions
+Vue.use(Resource)
+
+// routing
+var router = new VueRouter({
+  routes: [
+     {path: '/', component: HomeView},
+     {path: '/top-authors', component: AuthorsView}
+  ]
+})
+
+new Vue({
+  router
+}).$mount('#app')
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+/*
+ * new Vue({ el: '#app', template: '<App/>', components: { App } })
+ */
