61507f9906f3d78b60c33bceaf25316d800df8de
bmondolot <bmondolot@gmail.com>
2016-12-11 17:26:12 +0100

    First tests for the home page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <homeList></homeList>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import HomeList from './components/HomeList'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    HomeList
   }
 }
 </script>
